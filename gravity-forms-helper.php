f237ec1a2b707ac02f3b7f6ca5577b24c58c02cc
Jake Spurlock <jspurlock@makermedia.com>
2014-03-27 10:37:30 -0700

    Added sanitization, and some notes.

--- gravity-forms-helper.php
+++ gravity-forms-helper.php
@@ -1,18 +1,11 @@
 <?php
 /*
  Plugin Name: Gravity Forms Helper
- Plugin URI: http://github.com/whyisjake/
+ Plugin URI: https://github.com/whyisjake/Gravity-Forms-Helper
  Description: Allows for easy access to Gravity Forms data using a shortcode.
  Author: whyisjake
  Version: 0.5
  Author URI: http://jakespurlock.com/
- */
-
-/**
- * Gravity Forms!
- *
- * A class that will allow easy access to Gravity Forms data.
- *
  */
 
 /**
@@ -37,12 +30,22 @@ class Gravity_Forms_Helper {
 	}
 
 	public function get_form( $atts ) {
+
+		// Let's merge all of the attributes
 		extract( shortcode_atts( array(
-			'form'	=> 1,
-			'field'	=> 1,
+			'form'				=> 1,
+			'field'				=> 1,
+			'sanitize_callback'	=> 'wp_kses_post'
 		), $atts ) );
+
+		// Get the full form
 		$lead = RGFormsModel::get_lead( $form );
-		return $lead[ $field ];
+
+		// Based on the field, pull the value from the index.
+		$output = $lead[ $field ];
+
+		//Sanitize the function, we'll use wp_kses_post as it is pretty broad, but we can change the filter if desired.
+		return $sanitize_callback( $output );
 	}
 
 }
